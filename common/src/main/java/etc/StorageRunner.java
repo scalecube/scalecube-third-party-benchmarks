d31ff1652bf6f93fd5f83993e16f00323860436f
Dmytro Lazebnyi <dmytro.lazebnyi@jivygroup.com>
2018-05-04 16:41:46 +0300

    Use StorageWriter for ConcurrentHashMap test

--- common/src/main/java/etc/StorageRunner.java
+++ common/src/main/java/etc/StorageRunner.java
@@ -24,56 +24,36 @@ public class StorageRunner {
 
         Storage<Integer, Order> storage = storage();
 
-        IntStream.rangeClosed(1, RECORDS_CNT).forEach(i -> {
-            Order order1 = new Order();
-            try {
-                storage.write(order1.getId(), order1);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        StorageWriter writer = new StorageWriter(RECORDS_CNT, storage, timeFullWrite);
+        writer.populate();
+        writer.write();
 
+        //TODO: move to StorageReader
         ThreadLocalRandom rnd = ThreadLocalRandom.current();
         IntStream.rangeClosed(1, RECORDS_CNT).forEach(i -> {
-                    int idx = rnd.nextInt(1, RECORDS_CNT);
-                    Timer.Context readTime = timeFullRead.time();
-                    try {
-                        storage.read(idx);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    readTime.stop();
+                int idx = rnd.nextInt(1, RECORDS_CNT);
+                Timer.Context readTime = timeFullRead.time();
+                try {
+                    storage.read(idx);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
+                readTime.stop();
+            }
         );
-
-        // Write to full
-        IntStream.rangeClosed(1, RECORDS_CNT).forEach(i -> {
-            Order order = new Order();
-            Timer.Context time = timeFullWrite.time();
-            try {
-                storage.write(order.getId(), order);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            time.stop();
-            if (i == RECORDS_CNT) {
-                reporter.stop();
-                System.out.println("DONE");
-            }
-        });
 
         //        Thread.currentThread().join();
     }
 
     private static Storage<Integer, Order> storage() {
-        return new HashMapStorage(RECORDS_CNT * 2);
+        return new HashMapStorage<>(RECORDS_CNT * 2);
     }
 
     private static class HashMapStorage<K, V> implements Storage<K, V> {
 
         private ConcurrentHashMap<K, V> map;
 
-        public HashMapStorage(int capacity) {
+        HashMapStorage(int capacity) {
             this.map = new ConcurrentHashMap<>(capacity);
         }
 
