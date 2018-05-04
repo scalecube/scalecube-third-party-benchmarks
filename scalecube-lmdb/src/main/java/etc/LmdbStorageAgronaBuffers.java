e4784c63145b018ed4d54e18bae2f16413db3f39
Sergii Ripa <sergey.ripa@jivygroup.com>
2018-05-04 17:57:40 +0300

    tune lmdb for inserts

--- scalecube-lmdb/src/main/java/etc/LmdbStorageAgronaBuffers.java
+++ scalecube-lmdb/src/main/java/etc/LmdbStorageAgronaBuffers.java
@@ -3,8 +3,11 @@ package etc;
 import org.agrona.DirectBuffer;
 import org.agrona.MutableDirectBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
+import org.lmdbjava.Cursor;
 import org.lmdbjava.Dbi;
 import org.lmdbjava.Env;
+import org.lmdbjava.EnvFlags;
+import org.lmdbjava.Txn;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,19 +27,29 @@ public class LmdbStorageAgronaBuffers implements Storage<Integer, Order> {
         env = Env.create(PROXY_DB)
                 .setMapSize(10_485_760_000l)
                 .setMaxDbs(1)
-                .open(path);
+                .open(path, EnvFlags.MDB_NOSYNC, EnvFlags.MDB_WRITEMAP);
 
         db = env.openDbi(DB_NAME, MDB_CREATE);
+        System.getProperties().setProperty(Env.DISABLE_CHECKS_PROP, "true");
     }
 
     @Override
     public void write(Integer k, Order v) throws IOException {
-        final MutableDirectBuffer key = new UnsafeBuffer(allocateDirect(4));
-        key.putInt(0, v.getId());
-        byte[] valBytes = v.serialized();
-        final MutableDirectBuffer val = new UnsafeBuffer(allocateDirect(valBytes.length));
-        val.putBytes(0, valBytes);
-        db.put(key, val);
+        try (Txn<DirectBuffer> txn = env.txnWrite()) {
+            final Cursor<DirectBuffer> c = db.openCursor(txn);
+
+            final MutableDirectBuffer key = new UnsafeBuffer(allocateDirect(4));
+            key.putInt(0, v.getId());
+
+            byte[] valBytes = v.serialized();
+            final MutableDirectBuffer val = new UnsafeBuffer(allocateDirect(valBytes.length));
+            val.putBytes(0, valBytes);
+            c.put(key, val);
+
+            c.close();
+            txn.commit();
+        }
+
     }
 
     @Override
