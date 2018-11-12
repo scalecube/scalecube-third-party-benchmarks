d325bd92258a68967c6e5f0a25355b850239174e
artem-v <artem.vysochyn@gmail.com>
2018-11-12 23:16:33 +0200

    Refactored LocalDAteTime marshalling to be in synk with jdk's localdate time serialization

--- common/src/main/java/io/scalecube/storages/common/entity/LocalDateTimeUtil.java
+++ common/src/main/java/io/scalecube/storages/common/entity/LocalDateTimeUtil.java
@@ -6,8 +6,7 @@ import java.time.LocalTime;
 import net.openhft.chronicle.bytes.Bytes;
 
 /**
- * todo see https://github.com/OpenHFT/Chronicle-Wire.
- * {@link net.openhft.chronicle.wire.RawWire.RawValueOut#dateTime(java.time.LocalDateTime)}
+ * todo see https://github.com/OpenHFT/Chronicle-Wire. {@link net.openhft.chronicle.wire.RawWire.RawValueOut#dateTime(java.time.LocalDateTime)}
  */
 public final class LocalDateTimeUtil {
 
@@ -18,8 +17,8 @@ public final class LocalDateTimeUtil {
    * @param out output
    */
   public static void writeObject(LocalDateTime value, Bytes out) {
-    out.writeStopBit(value.toLocalDate().toEpochDay());
-    out.writeLong(value.toLocalTime().toNanoOfDay());
+    writeLocalDate(out, value.toLocalDate());
+    writeLocalTime(out, value.toLocalTime());
   }
 
   /**
@@ -29,9 +28,70 @@ public final class LocalDateTimeUtil {
    * @return LocalDateTime value
    */
   public static LocalDateTime readObject(Bytes in) {
-    long epochDay = in.readStopBit();
-    long nanoOfDay = in.readLong();
+    LocalDate date = readLocalDate(in);
+    LocalTime time = readLocalTime(in);
+    return LocalDateTime.of(date, time);
+  }
 
-    return LocalDateTime.of(LocalDate.ofEpochDay(epochDay), LocalTime.ofNanoOfDay(nanoOfDay));
+  private static LocalDate readLocalDate(Bytes in) {
+    int year = in.readInt();
+    int month = in.readByte();
+    int dayOfMonth = in.readByte();
+    return LocalDate.of(year, month, dayOfMonth);
+  }
+
+  private static LocalTime readLocalTime(Bytes in) {
+    int hour = in.readByte();
+    int minute = 0;
+    int second = 0;
+    int nano = 0;
+    if (hour < 0) {
+      hour = ~hour;
+    } else {
+      minute = in.readByte();
+      if (minute < 0) {
+        minute = ~minute;
+      } else {
+        second = in.readByte();
+        if (second < 0) {
+          second = ~second;
+        } else {
+          nano = in.readInt();
+        }
+      }
+    }
+    return LocalTime.of(hour, minute, second, nano);
+  }
+
+  private static void writeLocalDate(Bytes out, LocalDate localDate) {
+    out.writeInt(localDate.getYear());
+    out.writeByte((byte) localDate.getMonthValue());
+    out.writeByte((byte) localDate.getDayOfMonth());
+  }
+
+  private static void writeLocalTime(Bytes out, LocalTime localTime) {
+    int nano = localTime.getNano();
+    int second = localTime.getSecond();
+    int minute = localTime.getMinute();
+    int hour = localTime.getHour();
+    if (nano == 0) {
+      if (second == 0) {
+        if (minute == 0) {
+          out.writeByte((byte) ~hour);
+        } else {
+          out.writeByte((byte) hour);
+          out.writeByte((byte) ~minute);
+        }
+      } else {
+        out.writeByte((byte) hour);
+        out.writeByte((byte) minute);
+        out.writeByte((byte) ~second);
+      }
+    } else {
+      out.writeByte((byte) hour);
+      out.writeByte((byte) minute);
+      out.writeByte((byte) second);
+      out.writeInt(nano);
+    }
   }
 }
